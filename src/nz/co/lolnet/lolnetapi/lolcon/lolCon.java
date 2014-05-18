3fb61986ee7f775e47c6d76e42b92c400e0d5ecf
D Win <dwin@wintech.net.nz>
2014-05-18 18:00:45 +1200

    GetForumGroups

--- src/nz/co/lolnet/lolnetapi/lolcon/lolCon.java
+++ src/nz/co/lolnet/lolnetapi/lolcon/lolCon.java
@@ -3,8 +3,12 @@ package nz.co.lolnet.lolnetapi.lolcon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import org.json.simple.JSONObject;
@@ -40,6 +44,38 @@ public class lolCon {
         }
         
         return output;        
+    }
+    
+    public static HashMap<String, Integer> getForumGroups(String authHash) throws UnsupportedEncodingException, MalformedURLException, IOException, ParseException {
+        HashMap<String, Integer> output = new HashMap<>();   
+        
+        String data = URLEncoder.encode("authhash", "UTF-8") + "=" + URLEncoder.encode(authHash, "UTF-8");
+
+        // Send data
+        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolcoins/getforumgroups.php");
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        conn.setConnectTimeout(5000);
+        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        wr.write(data);
+        wr.flush();
+
+        // Get the response
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        JSONObject json = (JSONObject) new JSONParser().parse(rd.readLine());
+
+        wr.close();
+        rd.close();
+
+        Iterator<?> keys = json.keySet().iterator();
+
+        while(keys.hasNext()) {
+            String group_id = (String) keys.next();
+            String group_name = (String) json.get(group_id);
+            output.put(group_name, Integer.parseInt(group_id));
+        }
+        
+        return output;
     }
     
 }
