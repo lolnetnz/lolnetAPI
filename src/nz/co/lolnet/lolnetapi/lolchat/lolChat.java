de9fd9c3048b454aadcb224b08e95ce405200331
D Win <dwin@wintech.net.nz>
2014-05-22 16:53:13 +1200

    recordMessage

---
+++ src/nz/co/lolnet/lolnetapi/lolchat/lolChat.java
@@ -0,0 +1,35 @@
+package nz.co.lolnet.lolnetapi.lolchat;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import nz.co.lolnet.lolnetAPI;
+
+/**
+ *
+ * @author cptwin
+ */
+public class lolChat {
+    
+    public static void recordMessage(String authHash, String servername, String playername, String message) throws UnsupportedEncodingException, MalformedURLException, IOException
+    {
+        String data = URLEncoder.encode("authhash", "UTF-8") + "=" + URLEncoder.encode(authHash, "UTF-8");
+        data += "&" + URLEncoder.encode("servername", "UTF-8") + "=" + URLEncoder.encode(servername, "UTF-8");
+        data += "&" + URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(playername, "UTF-8");
+        data += "&" + URLEncoder.encode("message", "UTF-8") + "=" + URLEncoder.encode(message, "UTF-8");
+            
+        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolchat/recordmessage.php");
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        conn.setConnectTimeout(lolnetAPI.httpTimeOut);
+        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        wr.write(data);
+        wr.flush();
+        wr.close();
+    }
+    
+}
