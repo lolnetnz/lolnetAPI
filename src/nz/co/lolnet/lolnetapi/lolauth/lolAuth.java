44b6cce9d64546d06d600576ba131a1dfc2418f0
D Win <dwin@wintech.net.nz>
2014-05-24 23:10:41 +1200

    Fixed login system

--- src/nz/co/lolnet/lolnetapi/lolauth/lolAuth.java
+++ src/nz/co/lolnet/lolnetapi/lolauth/lolAuth.java
@@ -7,6 +7,7 @@ import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import nz.co.lolnet.lolnetAPI;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -21,26 +22,27 @@ public class lolAuth {
      * Attempts to login a player against the lolnet database
      * 
      * @version 1.0 17/05/2014 - First Added
+     * @version 2.0 24/05/2014 - Comparison with hash rather than checking directly against database
+     * @param authHash
      * @param playerName
      * @param password
      * @return true if login was successful
      * @throws IOException
      * @throws ParseException 
      */
-    public static boolean login(String playerName, String password) throws IOException, ParseException {
+    public static boolean login(String authHash, String playerName, String password) throws IOException, ParseException {
         PhpbbHandler p = new PhpbbHandler();
 
         String player = playerName.toLowerCase();
-        String passwordHash = p.phpbb_hash(password);
 
-        String data = URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(player, "UTF-8");
-        data += "&" + URLEncoder.encode("passwordhash", "UTF-8") + "=" + URLEncoder.encode(passwordHash, "UTF-8");
+        String data = URLEncoder.encode("authhash", "UTF-8") + "=" + URLEncoder.encode(authHash, "UTF-8");
+        data += "&" + URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(player, "UTF-8");
 
         // Send data
-        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolauth/login.php");
+        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolauth/gethash.php");
         URLConnection conn = url.openConnection();
         conn.setDoOutput(true);
-        conn.setConnectTimeout(10000);
+        conn.setConnectTimeout(lolnetAPI.httpTimeOut);
         OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
         wr.write(data);
         wr.flush();
@@ -50,7 +52,10 @@ public class lolAuth {
         JSONObject json = (JSONObject) new JSONParser().parse(rd.readLine());
         wr.close();
         rd.close();
-        return "true".equals(json.get("loggedin").toString());
+        
+        String hash = (String) json.get("hash");
+        
+        return p.phpbb_check_hash(password, hash);
     }
 
 }
