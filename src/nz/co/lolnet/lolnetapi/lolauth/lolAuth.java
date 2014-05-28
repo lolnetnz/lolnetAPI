25e20e8520fe85a5bea02540430b7437c0621efc
D Win <dwin@wintech.net.nz>
2014-05-28 16:53:34 +1200

    register

--- src/nz/co/lolnet/lolnetapi/lolauth/lolAuth.java
+++ src/nz/co/lolnet/lolnetapi/lolauth/lolAuth.java
@@ -4,9 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.zip.CRC32;
 import nz.co.lolnet.lolnetAPI;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -31,7 +34,7 @@ public class lolAuth {
      * @throws ParseException 
      */
     public static boolean login(String authHash, String playerName, String password) throws IOException, ParseException {
-        PhpbbHandler p = new PhpbbHandler();
+        PhpbbHandler phpbbhandler = new PhpbbHandler();
 
         String player = playerName.toLowerCase();
 
@@ -55,7 +58,58 @@ public class lolAuth {
         
         String hash = (String) json.get("hash");
         
-        return p.phpbb_check_hash(password, hash);
+        return phpbbhandler.phpbb_check_hash(password, hash);
+    }
+    
+    public static boolean register(String authHash, String playerName, String email, String ip, String password) throws UnsupportedEncodingException, MalformedURLException, IOException, ParseException
+    {
+        PhpbbHandler phpbbhandler = new PhpbbHandler();
+        
+        //Prepare all data needed to register
+        String passwordHash = phpbbhandler.phpbb_hash(password);
+        long timestamp = System.currentTimeMillis() / 1000;
+        String playerIP = cleanIpAddress(ip);
+        long emailHash = emailHash(email);
+        
+        String data = URLEncoder.encode("authhash", "UTF-8") + "=" + URLEncoder.encode(authHash, "UTF-8");
+        data += "&" + URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(playerName, "UTF-8");
+        data += "&" + URLEncoder.encode("player_password", "UTF-8") + "=" + URLEncoder.encode(passwordHash, "UTF-8");
+        data += "&" + URLEncoder.encode("user_email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+        data += "&" + URLEncoder.encode("timestamp", "UTF-8") + "=" + URLEncoder.encode(timestamp + "", "UTF-8");
+        data += "&" + URLEncoder.encode("player_ip", "UTF-8") + "=" + URLEncoder.encode(playerIP, "UTF-8");
+        data += "&" + URLEncoder.encode("email_hash", "UTF-8") + "=" + URLEncoder.encode(emailHash + "", "UTF-8");
+        
+        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolauth/register.php");
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        conn.setConnectTimeout(lolnetAPI.httpTimeOut);
+        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        wr.write(data);
+        wr.flush();
+
+        // Get the response
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        JSONObject json = (JSONObject) new JSONParser().parse(rd.readLine());
+        
+        wr.close();
+        rd.close();
+        
+        return (boolean) json.get("success");
+    }
+    
+    private static String cleanIpAddress(String ip) {
+        String output = ip;
+        output = output.replaceAll("/", "");
+        String[] str_array = output.split(":");
+        output = str_array[0];
+        return output;
+    }
+    
+    private static long emailHash(String email) {
+        CRC32 crcMaker = new CRC32();
+        crcMaker.update(email.getBytes());
+        long crc = crcMaker.getValue();
+        return crc + email.length();
     }
 
 }
