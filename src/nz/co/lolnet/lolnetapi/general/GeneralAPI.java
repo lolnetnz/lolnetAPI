1c42930b3cd87010e9b9a9e9bb181e53b9eb35fa
D Win <dwin@wintech.net.nz>
2014-09-05 18:05:25 +1200

    GetUUID

--- src/nz/co/lolnet/lolnetapi/general/GeneralAPI.java
+++ src/nz/co/lolnet/lolnetapi/general/GeneralAPI.java
@@ -3,8 +3,10 @@ package nz.co.lolnet.lolnetapi.general;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import nz.co.lolnet.lolnetAPI;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -34,6 +36,41 @@ public class GeneralAPI {
         rd.close();       
         String status = (String) json.get("session.minecraft.net");       
         return status.equalsIgnoreCase("green");
+    }
+    
+    /**
+     * Grabs the UUID of a Player.
+     * @param playerName
+     * @return String Representation of a UUID, which you can then use UUID.fromString() on.
+     * @throws IOException
+     * @throws ParseException 
+     */
+    public static String getStringUUID(String playerName) throws IOException, ParseException {
+
+        String player = playerName.toLowerCase();
+
+        String data = URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(player, "UTF-8");
+        data += "&" + URLEncoder.encode("currenttime", "UTF-8") + "=" + URLEncoder.encode(System.currentTimeMillis() + "", "UTF-8");
+
+        // Send data
+        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/getplayeruuid.php");
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        conn.setConnectTimeout(lolnetAPI.httpTimeOut);
+        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        wr.write(data);
+        wr.flush();
+
+        // Get the response
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        JSONObject json = (JSONObject) new JSONParser().parse(rd.readLine());
+        wr.close();
+        rd.close();
+        
+        String rawUUID = (String) json.get("id");  
+        String UUID = rawUUID.substring(0, 8) + "-" + rawUUID.substring(8, 12) + "-" + rawUUID.substring(12, 16) + "-" + rawUUID.substring(16, 20) + "-" +rawUUID.substring(20, 32);
+        
+        return UUID;
     }
     
 }
