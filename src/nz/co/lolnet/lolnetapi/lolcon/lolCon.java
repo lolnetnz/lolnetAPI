4b5ecc15e099231709eecd2f67d072edf9bb8e18
James Anderson <james137137@gmail.com>
2015-06-15 13:54:28 +1200

    Added nick API (get and set)

--- src/nz/co/lolnet/lolnetapi/lolcon/lolCon.java
+++ src/nz/co/lolnet/lolnetapi/lolcon/lolCon.java
@@ -154,6 +154,60 @@ public class lolCon {
         return result;
     }
     
+    public static String updatetPlayerNick(String playername) throws UnsupportedEncodingException, IOException, ParseException {
+        String data = URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(playername, "UTF-8");
+
+        URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolcoins/getplayernick.php");
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        conn.setConnectTimeout(lolnetAPI.httpTimeOut);
+        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        wr.write(data);
+        wr.flush();
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        JSONObject json = (JSONObject) new JSONParser().parse(rd.readLine());
+
+        wr.close();
+        rd.close();
+
+        return (String) json.get("playerTitle");
+    }
+    
+    public static boolean updatetPlayerNick(String playerName, String newNick,String authhash) {
+        boolean result = false;
+        try {
+            // Construct data
+
+            String data = URLEncoder.encode("playername", "UTF-8") + "=" + URLEncoder.encode(playerName, "UTF-8");
+            data += "&" + URLEncoder.encode("authhash", "UTF-8") + "=" + URLEncoder.encode(authhash, "UTF-8");
+            data += "&" + URLEncoder.encode("newtitle", "UTF-8") + "=" + URLEncoder.encode(newNick, "UTF-8");
+            // Send data
+            URL url = new URL("https://www.lolnet.co.nz/api/v1.0/lolcoins/updateplayernick.php");
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            // Get the response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = null;
+            while ((line = rd.readLine()) != null) {
+                if (line.toLowerCase().contains("true")) {
+                    result = true;
+                    break;
+                }
+            }
+            wr.close();
+            rd.close();
+        }
+        catch (Exception e) {
+            return result;
+        }
+        return result;
+    }
+    
     public static HashMap<String, Long> getTop10MonthlyVoters() throws IOException, ParseException {
         HashMap<String, Long> output = new HashMap<>();
 
